c70199d1076d67706861a64d910d0e38eb5c65d8
Dmitry Tolmachev <todem5@gmail.com>
2016-07-27 23:02:55 +0600

    Initial Commit

---
+++ CinemaLogicTests/CinemaLogicTests.swift
@@ -0,0 +1,36 @@
+//
+//  CinemaLogicTests.swift
+//  CinemaLogicTests
+//
+//  Created by Dmitry Tolmachev on 27.07.16.
+//  Copyright © 2016 Dmitry Tolmachev. All rights reserved.
+//
+
+import XCTest
+@testable import CinemaLogic
+
+class CinemaLogicTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
